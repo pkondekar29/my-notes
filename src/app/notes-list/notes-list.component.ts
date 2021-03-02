cf9b0acfd3c1d7bf2eb5470c8f4434d79e9880bc
Palash Kondekar <palash.kondekar@sap.com>
2021-03-02 14:58:04 +0530

    Table Styling changes

--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -1,47 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { NotesService } from '../services/notes.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface NoteElement {
-  Note: string;
-  Tags: string[];
-  Course: string;
+   Note: string;
+   Tags: string[];
+   Course: string;
 }
 
 @Component({
-  selector: 'app-notes-list',
-  templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.css']
+   selector: 'app-notes-list',
+   templateUrl: './notes-list.component.html',
+   styleUrls: ['./notes-list.component.css']
 })
 export class NotesListComponent implements OnInit {
 
-  boundNotes: any[] = [];
-  notes: any[] = [];
-  filters: any[] = [];
-  courses: any[] = [];
-  displayedColumns: string[] = ['Note'];
+   dataSource = new MatTableDataSource();
+   notes: any[] = [];
+   filters: any[] = [];
+   courses: any[] = [];
+   displayedColumns: string[] = ['note', 'course'];
 
-  constructor(private noteService: NotesService) { }
+   constructor(private noteService: NotesService) { }
 
-  ngOnInit(): void {
-    // Inititalize the Nodes incoming from Service
-    this.noteService.getNotes()
-      .subscribe((results) => {
-        this.notes.push(results);
-        for(var i in results) {
-          if(!(results[i].Course in this.courses)) {
-            this.courses.push(results[i].Course);
-          }
-          for(var j in results[i].Tags) {
-            if(!(results[i].Tags[j] in this.filters)) {
-              this.filters.push(results[i].Tags[j]);
+   ngOnInit(): void {
+      // Inititalize the Nodes incoming from Service
+      this.noteService.getNotes()
+         .subscribe((results) => {
+            this.notes.push(results);
+            for (var i in results) {
+               if (!(results[i].Course in this.courses)) {
+                  this.courses.push(results[i].Course);
+               }
+               for (var j in results[i].Tags) {
+                  if (!(results[i].Tags[j] in this.filters)) {
+                     this.filters.push(results[i].Tags[j]);
+                  }
+               }
             }
-          }
-        }
-        
-        this.boundNotes = results;
-      }, (error) => {
-        console.error("Error orccured: " + error);
-      });
-  }
+
+            this.dataSource.data = results;
+         }, (error) => {
+            console.error("Error orccured: " + error);
+         });
+   }
 
 }
